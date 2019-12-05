50d81dcd261fa35be6775f24ec63a5b9721f6cdc
M3M0 <guillermo.cruz@sacbesolutions.net>
2019-12-04 22:14:26 -0600

    Creación tablas

--- database/migrations/2019_12_05_035254_create_table_loans.php
+++ database/migrations/2019_12_05_035254_create_table_loans.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
+class CreateTableLoans extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('test_loans', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->integer('loan_id');
+            $table->date('start_date');
+            $table->float('due');
+            $table->string('city');
+            $table->date('end_date');
+            $table->timestamps();
         });
     }
 
@@ -30,6 +31,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('test_loans');
     }
 }
