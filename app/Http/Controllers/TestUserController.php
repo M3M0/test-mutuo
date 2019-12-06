d4096e504490795414fba520ebf5b6fb8e990a72
M3M0 <guillermo.cruz@sacbesolutions.net>
2019-12-05 20:29:28 -0600

    Función Autenticar

--- app/Http/Controllers/TestUserController.php
+++ app/Http/Controllers/TestUserController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TestUsers;
 
 class TestUserController extends Controller
 {
-    //
+    public function auth(Request $request)
+    {
+        $status = false;
+        $message = '';
+        $res = '';
+        try{
+            $email = $request->input('email');
+            $pass = $request->input('pass');
+            $user = TestUsers::where([['password', $pass], ['email', $email]])->first();
+            // var_dump($user);
+            // die();
+            if($user){
+                $status = true;
+                $message = 'OK';
+                $res = $user->token;
+            }
+            else{
+                $message = 'No se pudo validar el usuario';
+            }
+        } catch(Exception $e){
+            $message = 'Error al validar el usuario';
+        }
+        return response()->json([
+            'status'    => $status,
+            'code'      => 200,
+            'message'   => $message,
+            'result'    => $res
+        ], 200);
+    }
 }
